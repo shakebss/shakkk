0990e405123380912b5df276d48d34eb17bd940f
Bob Long <robertlong13@gmail.com>
2022-07-27 22:06:22 -0400

    Overhaul QuickView editing dialog

---
+++ Controls/QuickViewOptions.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace MissionPlanner.Controls
+{
+    partial class QuickViewOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CMB_Source = new System.Windows.Forms.ComboBox();
+            this.NUM_precision = new System.Windows.Forms.NumericUpDown();
+            this.LBL_precision = new System.Windows.Forms.Label();
+            this.CHK_customcolor = new System.Windows.Forms.CheckBox();
+            this.TXT_color = new System.Windows.Forms.TextBox();
+            this.BUT_colorpicker = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NUM_precision)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CMB_Source
+            // 
+            this.CMB_Source.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.CMB_Source.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.CMB_Source.FormattingEnabled = true;
+            this.CMB_Source.Location = new System.Drawing.Point(12, 12);
+            this.CMB_Source.Name = "CMB_Source";
+            this.CMB_Source.Size = new System.Drawing.Size(121, 21);
+            this.CMB_Source.TabIndex = 1;
+            this.CMB_Source.SelectedIndexChanged += new System.EventHandler(this.CMB_Source_SelectedIndexChanged);
+            // 
+            // NUM_precision
+            // 
+            this.NUM_precision.Location = new System.Drawing.Point(197, 12);
+            this.NUM_precision.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.NUM_precision.Name = "NUM_precision";
+            this.NUM_precision.Size = new System.Drawing.Size(40, 20);
+            this.NUM_precision.TabIndex = 3;
+            this.NUM_precision.ValueChanged += new System.EventHandler(this.NUM_precision_ValueChanged);
+            // 
+            // LBL_precision
+            // 
+            this.LBL_precision.AutoSize = true;
+            this.LBL_precision.Location = new System.Drawing.Point(139, 15);
+            this.LBL_precision.Name = "LBL_precision";
+            this.LBL_precision.Size = new System.Drawing.Size(53, 13);
+            this.LBL_precision.TabIndex = 4;
+            this.LBL_precision.Text = "Precision:";
+            // 
+            // CHK_customcolor
+            // 
+            this.CHK_customcolor.AutoSize = true;
+            this.CHK_customcolor.Location = new System.Drawing.Point(246, 14);
+            this.CHK_customcolor.Name = "CHK_customcolor";
+            this.CHK_customcolor.Size = new System.Drawing.Size(88, 17);
+            this.CHK_customcolor.TabIndex = 5;
+            this.CHK_customcolor.Text = "Custom Color";
+            this.CHK_customcolor.UseVisualStyleBackColor = true;
+            this.CHK_customcolor.CheckedChanged += new System.EventHandler(this.CHK_customcolor_CheckedChanged);
+            // 
+            // TXT_color
+            // 
+            this.TXT_color.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TXT_color.Location = new System.Drawing.Point(335, 15);
+            this.TXT_color.Name = "TXT_color";
+            this.TXT_color.Size = new System.Drawing.Size(54, 13);
+            this.TXT_color.TabIndex = 6;
+            this.TXT_color.Text = "FFFFFF";
+            this.TXT_color.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TXT_color.TextChanged += new System.EventHandler(this.TXT_color_TextChanged);
+            // 
+            // BUT_colorpicker
+            // 
+            this.BUT_colorpicker.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BUT_colorpicker.Location = new System.Drawing.Point(400, 12);
+            this.BUT_colorpicker.Name = "BUT_colorpicker";
+            this.BUT_colorpicker.Size = new System.Drawing.Size(20, 20);
+            this.BUT_colorpicker.TabIndex = 7;
+            this.BUT_colorpicker.UseVisualStyleBackColor = false;
+            this.BUT_colorpicker.Click += new System.EventHandler(this.BUT_colorpicker_Click);
+            // 
+            // QuickViewOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 45);
+            this.Controls.Add(this.BUT_colorpicker);
+            this.Controls.Add(this.TXT_color);
+            this.Controls.Add(this.CHK_customcolor);
+            this.Controls.Add(this.LBL_precision);
+            this.Controls.Add(this.NUM_precision);
+            this.Controls.Add(this.CMB_Source);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "QuickViewOptions";
+            this.Text = "QuickViewOptions";
+            this.Shown += new System.EventHandler(this.QuickViewOptions_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.NUM_precision)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CMB_Source;
+        private System.Windows.Forms.NumericUpDown NUM_precision;
+        private System.Windows.Forms.Label LBL_precision;
+        private System.Windows.Forms.CheckBox CHK_customcolor;
+        private System.Windows.Forms.TextBox TXT_color;
+        private System.Windows.Forms.Button BUT_colorpicker;
+    }
+}
---
+++ Controls/QuickViewOptions.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner.Controls
+{
+    public partial class QuickViewOptions : Form
+    {
+        private QuickView _qv;
+        public QuickViewOptions(QuickView qv)
+        {
+            InitializeComponent();
+            _qv = qv;
+        }
+
+        private void QuickViewOptions_Shown(object sender, EventArgs e)
+        {
+            // Populate combobox with variables
+            List<string> options = new List<string>();
+            object defaultsrc = MainV2.comPort.MAV.cs;
+            Type test = defaultsrc.GetType();
+            foreach (var field in test.GetProperties())
+            {
+                // field.Name has the field's name.
+                object fieldValue;
+                TypeCode typeCode;
+                try
+                {
+                    fieldValue = field.GetValue(defaultsrc, null); // Get value
+
+                    if (fieldValue == null)
+                        continue;
+
+                    // Get the TypeCode enumeration. Multiple types get mapped to a common typecode.
+                    typeCode = Type.GetTypeCode(fieldValue.GetType());
+                }
+                catch
+                {
+                    continue;
+                }
+
+                options.Add(field.Name);
+            }
+            CMB_Source.DataSource = options;
+
+            // Initialize combobox selection
+            CMB_Source.Text = (string)_qv.Tag;
+
+            // Initialize precision field, , assuming numberformat follows 0 to 0.0000000 format
+            if (_qv.numberformat == "0")
+            {
+                NUM_precision.Value = 0;
+            }
+            else if (_qv.numberformat.StartsWith("0."))
+            {
+                NUM_precision.Value = _qv.numberformat.Substring(2).Count();
+            }
+            else
+            {
+                NUM_precision.Value = 2;
+            }
+
+            // Initialize custom color checkbox
+            CHK_customcolor.Checked = (Utilities.Settings.Instance[_qv.Name + "_color"] != null && Utilities.Settings.Instance[_qv.Name + "_color"] != "default");
+
+            // Initialize custom color textbox and color picker button
+            TXT_color.Enabled = CHK_customcolor.Checked;
+            BUT_colorpicker.Enabled = CHK_customcolor.Checked;
+            TXT_color.Text = _qv.numberColor.Name;
+            BUT_colorpicker.BackColor = _qv.numberColor;
+        }
+
+        private void CMB_Source_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Utilities.Settings.Instance[_qv.Name] = CMB_Source.Text;
+        }
+
+        private void NUM_precision_ValueChanged(object sender, EventArgs e)
+        {
+            if(NUM_precision.Value == 0)
+            {
+                Utilities.Settings.Instance[_qv.Name + "_format"] = "0";
+            }
+            else if (NUM_precision.Value >= 1)
+            {
+                Utilities.Settings.Instance[_qv.Name + "_format"] = "0." + new string('0', (int) NUM_precision.Value);
+            }
+            else // Edge case, should not happen; default to 0.00
+            {
+                Utilities.Settings.Instance[_qv.Name + "_format"] = "0.00";
+            }
+        }
+
+        private void CHK_customcolor_CheckedChanged(object sender, EventArgs e)
+        {
+            TXT_color.Enabled = CHK_customcolor.Checked;
+            BUT_colorpicker.Enabled = CHK_customcolor.Checked;
+            if (CHK_customcolor.Checked == false)
+            {
+                Utilities.Settings.Instance[_qv.Name + "_color"] = "default";
+            }
+            else
+            {
+                Utilities.Settings.Instance[_qv.Name + "_color"] = TXT_color.Text;
+            }
+        }
+
+        private void TXT_color_TextChanged(object sender, EventArgs e)
+        {
+            if (!CHK_customcolor.Checked)
+            {
+                return;
+            }
+            
+            if (Regex.Match(TXT_color.Text, "^(?:[0-9a-fA-F]{2}){3,4}$").Success)
+            {
+                BUT_colorpicker.BackColor = System.Drawing.ColorTranslator.FromHtml("#"+TXT_color.Text);
+                Utilities.Settings.Instance[_qv.Name + "_color"] = "#"+TXT_color.Text;
+            }
+            if (System.Drawing.Color.FromName(TXT_color.Text).IsKnownColor)
+            {
+                BUT_colorpicker.BackColor = System.Drawing.ColorTranslator.FromHtml(TXT_color.Text);
+                Utilities.Settings.Instance[_qv.Name + "_color"] = TXT_color.Text;
+            }
+        }
+
+        private void BUT_colorpicker_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            colorDlg.Color = BUT_colorpicker.BackColor;
+
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                BUT_colorpicker.BackColor = colorDlg.Color;
+                TXT_color.Text = colorDlg.Color.Name;
+            }
+        }
+    }
+}
